9ee30fbcb54bdddc5b642504fed11c8669af4108
João <joaolboing@gmail.com>
2018-12-18 15:01:47 -0200

    router, catch and error handler

--- src/index.js
+++ src/index.js
@@ -3,12 +3,32 @@ import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import cors from 'cors'
 
+import router from './router'
+
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(morgan('dev'))
+
+// Routes
+app.use('/', router)
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// Error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    status: 'error',
+    message: err.message
+  })
+})
 
 const PORT = process.env.PORT || 0
 app.listen(PORT, () => {
